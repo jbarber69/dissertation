8011a7e9aa923cb2e08b3395697f25d70f028c4b
Jack Barber <jbarber@solomonic.co.uk>
2021-03-04 18:53:26 +0000

    Auto-generated commit

--- assets/words.ts
+++ assets/words.ts
@@ -1,7 +1,7 @@
 export var italianWords: string[]  = [
-  'Ciao', 'Buonasera', 'Buonanotte'
+  'Ciao', 'Buonasera', 'Buonanotte', 'Buongiorno', 'Grazie mille', 'Piacere', 'Non capisco', 'Parla Inglese?'
 ]
 
 export var englishWords: string[] = [
-  'hello', 'good evening', 'good night'
+  'hello', 'good evening', 'good night', 'good morning', 'thank you very much', 'nice to meet you', `i don't understand`, 'do you speak english?'
 ]
